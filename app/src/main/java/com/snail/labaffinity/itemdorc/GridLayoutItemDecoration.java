897a34dc2fcd5e5317230852af9c603c01c55039
happysnail <happylishang@163.com>
2016-12-25 21:15:04 +0800

    撤销无用的绘制

--- app/src/main/java/com/snail/labaffinity/itemdorc/GridLayoutItemDecoration.java
+++ app/src/main/java/com/snail/labaffinity/itemdorc/GridLayoutItemDecoration.java
@@ -1,11 +1,6 @@
 package com.snail.labaffinity.itemdorc;
 
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.drawable.ShapeDrawable;
-import android.support.annotation.ColorInt;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -22,16 +17,10 @@ public class GridLayoutItemDecoration extends RecyclerView.ItemDecoration {
     private int mSpanCount;
     private int mHorizonSpan;
     private int mVerticalSpan;
-    private ShapeDrawable drawable;
-    private Paint mPaint;
     private GridLayoutManager mGridLayoutManager;
 
     public GridLayoutItemDecoration(int count) {
         mSpanCount = count;
-        drawable = new ShapeDrawable();
-        mPaint = new Paint();
-        mPaint = drawable.getPaint();
-        mPaint.setColor(0x00000000);
         setMargin(20, 20);
     }
 
@@ -41,60 +30,11 @@ public class GridLayoutItemDecoration extends RecyclerView.ItemDecoration {
     }
 
     @Override
-    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
-        drawHorizontal(c, parent);
-        drawVertical(c, parent);
-    }
-
-    public void drawVertical(Canvas c, RecyclerView parent) {
-
-        int totalCount = parent.getAdapter().getItemCount();
-        final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            final View child = parent.getChildAt(i);
-            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
-                    .getLayoutParams();
-            final int top = child.getBottom() + params.bottomMargin +
-                    Math.round(ViewCompat.getTranslationY(child));
-            final int bottom = top + mVerticalSpan;
-
-            final int left = child.getLeft() + params.leftMargin;
-            final int right = child.getRight() + params.rightMargin;
-
-            if (!isLastRaw(parent, i, mSpanCount, totalCount))
-                if (childCount - i > mSpanCount) {
-                    drawable.setBounds(left, top, right, bottom);
-                    drawable.draw(c);
-                }
-        }
-    }
-
-    public void drawHorizontal(Canvas c, RecyclerView parent) {
-        final int childCount = parent.getChildCount();
-        final int totalCount = parent.getAdapter().getItemCount();
-        for (int i = 0; i < childCount; i++) {
-            final View child = parent.getChildAt(i);
-            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
-                    .getLayoutParams();
-            final int left = child.getRight() + params.rightMargin +
-                    Math.round(ViewCompat.getTranslationX(child));
-            final int top = child.getTop() + params.topMargin;
-            final int bottom = child.getBottom() + params.bottomMargin;
-            final int right = left + mHorizonSpan;
-            if (!isLastColumn(parent, i, mSpanCount, totalCount)) {
-                drawable.setBounds(left, top, right, bottom);
-                drawable.draw(c);
-            }
-        }
-    }
-
-    @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         final int position = parent.getChildAdapterPosition(view);
         final int totalCount = parent.getAdapter().getItemCount();
-        int everyCharge = mHorizonSpan * (mSpanCount-1)/ (mSpanCount);
+        int everyCharge = mHorizonSpan * (mSpanCount - 1) / (mSpanCount);
         int modValue = position % mSpanCount;
-        //(0 4/5)|(1/5 3/5)|(2/5 2/5)|(3/5 1/5)|(4/5 0)
         final int left = Math.round(modValue * mHorizonSpan / mSpanCount);
         final int right = everyCharge - left;
 
@@ -108,41 +48,6 @@ public class GridLayoutItemDecoration extends RecyclerView.ItemDecoration {
     public void setMargin(int horizion, int vertical) {
         mHorizonSpan = horizion;
         mVerticalSpan = vertical;
-    }
-
-    public void setPaddingColor(@ColorInt int color) {
-        mPaint.setColor(color);
-    }
-
-    @Override
-    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
-        super.onDrawOver(c, parent, state);
-        // 添加浮层
-    }
-
-    private boolean isLastColumn(RecyclerView parent, int pos, int spanCount,
-                                 int childCount) {
-        RecyclerView.LayoutManager layoutManager = parent.getLayoutManager();
-        if (layoutManager instanceof GridLayoutManager) {
-            if ((pos + 1) % spanCount == 0)// 如果是最后一列，则不需要绘制右边
-            {
-                return true;
-            }
-        } else if (layoutManager instanceof StaggeredGridLayoutManager) {
-            int orientation = ((StaggeredGridLayoutManager) layoutManager)
-                    .getOrientation();
-            if (orientation == StaggeredGridLayoutManager.VERTICAL) {
-                if ((pos + 1) % spanCount == 0)// 如果是最后一列，则不需要绘制右边
-                {
-                    return true;
-                }
-            } else {
-                childCount = childCount - childCount % spanCount;
-                if (pos >= childCount)// 如果是最后一列，则不需要绘制右边
-                    return true;
-            }
-        }
-        return false;
     }
 
     private boolean isLastRaw(RecyclerView parent, int pos, int spanCount,
--- app/src/main/java/com/snail/labaffinity/itemdorc/LinearItemDecoration.java
+++ app/src/main/java/com/snail/labaffinity/itemdorc/LinearItemDecoration.java
@@ -1,10 +1,6 @@
 package com.snail.labaffinity.itemdorc;
 
-import android.graphics.Canvas;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -19,12 +15,10 @@ public class LinearItemDecoration extends RecyclerView.ItemDecoration {
 
     public static final int HORIZONTAL_LIST = LinearLayoutManager.HORIZONTAL;
     public static final int VERTICAL_LIST = LinearLayoutManager.VERTICAL;
-    private Drawable mDivider;
     private int mOrientation;
     private int mSpanSpace = 20;
 
     public LinearItemDecoration(int orientation) {
-        mDivider = new ShapeDrawable();
         setOrientation(orientation);
     }
 
@@ -38,54 +32,6 @@ public class LinearItemDecoration extends RecyclerView.ItemDecoration {
             throw new IllegalArgumentException("invalid orientation");
         }
         mOrientation = orientation;
-    }
-
-    @Override
-    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
-        if (mOrientation == VERTICAL_LIST) {
-            drawVertical(c, parent);
-        } else {
-            drawHorizontal(c, parent);
-        }
-    }
-
-    private void drawVertical(Canvas c, RecyclerView parent) {
-        final int left = parent.getPaddingLeft();
-        final int right = parent.getWidth() - parent.getPaddingRight();
-        final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            final View child = parent.getChildAt(i);
-            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
-                    .getLayoutParams();
-            final int top = child.getBottom() + params.bottomMargin +
-                    Math.round(ViewCompat.getTranslationY(child));
-            final int bottom = top + mSpanSpace;
-            if (i < parent.getAdapter().getItemCount() - 1) {
-                mDivider.setBounds(left, top, right, bottom);
-                mDivider.draw(c);
-            } else {
-                mDivider.setBounds(left, top, right, top);
-                mDivider.draw(c);
-            }
-        }
-    }
-
-    private void drawHorizontal(Canvas c, RecyclerView parent) {
-        final int top = parent.getPaddingTop();
-        final int bottom = parent.getHeight() - parent.getPaddingBottom();
-        final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            final View child = parent.getChildAt(i);
-            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
-                    .getLayoutParams();
-            final int left = child.getRight() + params.rightMargin +
-                    Math.round(ViewCompat.getTranslationX(child));
-            final int right = left + mSpanSpace;
-            if (i < parent.getAdapter().getItemCount() - 1) {
-                mDivider.setBounds(left, top, right, bottom);
-                mDivider.draw(c);
-            }
-        }
     }
 
     @Override
